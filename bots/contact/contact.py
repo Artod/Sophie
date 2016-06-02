90922ce0c95a91c6b7e34d755d35e2567c6d3f67
kushquad <kushquad@yahoo.com>
2016-06-02 14:09:54 +0530

    Updated with some examples

---
+++ bots/contact/contact.py
@@ -0,0 +1,14 @@
+# Main implementation file
+
+# Sophie interacts with this Python file to call the respective actions
+# Mapping the various parse lists with actions are done by the main Sophie bot
+# This simply contains data for Sophie to load into the core data structures
+# It also contains method definitions to be invoked by the core Sophie bot.
+
+# The metadata files contain information which are supplied while building entity, intent and action 
+# lists to assist the user while making custom response/action mappings.
+
+# Add data to Sophie here
+# Action definitions follow here
+# Sophie imports all bots from a botlist dynamically.
+# This botlist is updated everytime automatically a new bot is added/discovered to the system.
